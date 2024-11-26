1cb248e327194c3553c2c224ab9eb2d05f4916bc
diana <daiana.rehan.dr@gmail.com>
2024-11-26 23:28:26 +0200

    main for testing purposes

--- src/main/java/Core/Storage/ReservationStationEntry.java
+++ src/main/java/Core/Storage/ReservationStationEntry.java
@@ -3,15 +3,15 @@ package Core.Storage;
 public class ReservationStationEntry extends ReservationStationBaseEntry {
     private Object vj;
     private Object vk;
-    private String qj;
-    private String qk;
+    private String qj="0";
+    private String qk="0";
 
     public ReservationStationEntry(String name) {
         super(name);
         this.vj = null;
         this.vk = null;
-        this.qj = "";
-        this.qk = "";
+        this.qj = "0";
+        this.qk = "0";
     }
 
     public void setVj(Object value) {
@@ -40,5 +40,14 @@ public class ReservationStationEntry extends ReservationStationBaseEntry {
     }
     public String getQk() {
         return qk;
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        this.vj = null;
+        this.vk = null;
+        this.qj = "0";
+        this.qk = "0";
     }
 }
